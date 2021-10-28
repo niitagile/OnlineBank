755230e6bfdd7635e7e649eb211d0a0ecf6e4cd2
niitagile <43024343+niitagile@users.noreply.github.com>
2021-10-28 15:24:20 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package BasicConcepts;
+class Employee{}
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*float a=5.0f;
+		int b=2;8
+		int ans=(int) (a/b);*/
+		Employee emp=new Employee();
+		System.out.println(emp);
+	}
+
+}
